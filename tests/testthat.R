70986f1a3fe46ba4b891bc51ab0e7050179c9b14
Alexander-Gebreamlak <alex.gebreamlak@mail.utoronto.ca>
2024-10-24 20:07:21 -0400

    Update test

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,4 +9,7 @@
 library(testthat)
 library(AlexGebOR)
 
-test_check("AlexGebOR")
+test_that("OR and CI output are correct", {
+  expect_equal(OR_95CI(2, 0.5, 0.05, 2), "7.39 (2.77, 19.69)")
+  expect_error(OR_95CI("a", 0.5, 0.05, 2), "All inputs must be numeric")
+})
